128f13fc70a1531c8beb2c167dc2be5efeb61bee
varisbhalala <varisbhalala555@gmail.com>
2018-02-21 14:35:04 +0530

    Added the retrieve floor image module. TODO: POI store and retrieve.

--- js/AdminPanelAPI.js
+++ js/AdminPanelAPI.js
@@ -6,3 +6,5 @@ var FETCH_ALL_BUILDING = BASE_URL+"/anyplace/mapping/building/all";
 var ADD_BUILDING = BASE_URL+"/anyplace/mapping/building/add";
 var FLOOR_PLAN_UPLOAD = BASE_URL + "/anyplace/mapping/floor/upload";
 var ADD_FLOOR = BASE_URL + "/anyplace/mapping/floor/add";
+var FETCH_ALL_FLOORS = BASE_URL + "/anyplace/mapping/floor/all"
+var GET_FLOOR_IMAGE = BASE_URL + "/anyplace/floorplans64"

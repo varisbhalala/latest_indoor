b899ca889057d0fb8acea9f7bfa4cb030e9b8ff2
varisbhalala <varisbhalala555@gmail.com>
2018-02-23 21:48:24 +0530

    done

--- js/AdminPanelAPI.js
+++ js/AdminPanelAPI.js
@@ -1,4 +1,4 @@
-var BASE_URL="http://localhost:9000";
+var BASE_URL="http://34.210.58.101:9000";
 //var BASE_URL="http://SERVER IP:9000";
 
 
@@ -8,5 +8,8 @@ var FLOOR_PLAN_UPLOAD = BASE_URL + "/anyplace/mapping/floor/upload";
 var ADD_FLOOR = BASE_URL + "/anyplace/mapping/floor/add";
 var FETCH_ALL_FLOORS = BASE_URL + "/anyplace/mapping/floor/all"
 var GET_FLOOR_IMAGE = BASE_URL + "/anyplace/floorplans64"
-var ADD_POI = BASE_URL + "" //TODO: Add the URL to add POI here.
-var FETCH_ALL_POI = BASE_URL + "" //TODO: Add the URL to fetch the POIs here.
+var ADD_POI = BASE_URL + "/anyplace/mapping/pois/add" //TODO: Add the URL to add POI here.
+var FETCH_ALL_POI = BASE_URL + "/anyplace/mapping/pois/all_floor" //TODO: Add the URL to fetch the POIs here.
+var DELETE_BUILDING = BASE_URL + "/anyplace/mapping/building/delete"
+var DELETE_FLOOR = BASE_URL + "/anyplace/mapping/floor/delete"
+var DELETE_POI = BASE_URL + "/anyplace/mapping/pois/delete"
